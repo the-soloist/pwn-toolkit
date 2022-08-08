655cfd09c232035cb525641635edfff32cc88029
the-soloist <46804083+the-soloist@users.noreply.github.com>
2022-08-09 00:28:49 +0800

    Remove toplevel.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,22 @@
-from __future__ import absolute_import
-from pwn_utils.toplevel import *
+import pwn_utils as pwnutils
+from pwn_utils.lib.logger import plog, tlog
+
+
+try:
+    from pwn_utils.lib import parser
+    from pwn_utils.lib.entry import pwnpwnpwn
+    from pwn_utils.lib.tools import *
+except Exception as e:
+    plog.error(e)
+
+
+try:
+    import traceback
+    import ipdb
+    from tqdm import tqdm
+except Exception as e:
+    plog.warn(e)
+
+
+# alias
+pwn_the_world = pwnpwnpwn
