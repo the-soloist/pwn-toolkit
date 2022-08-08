0e9398d63cdb73c74c9417fce52860a101254e4a
the-soloist <46804083+the-soloist@users.noreply.github.com>
2022-08-09 00:08:08 +0800

    Auto set context.terminal

--- lib/debug.py
+++ lib/debug.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pwn import log, gdb, u32, u64, tube, pause
+from pwn import log, gdb, u32, u64, tube, pause, context
+import os
 
 
 def pdebug(sh, gdbscript="", bpl=[], gds={}):
@@ -29,38 +30,42 @@ def pdebug(sh, gdbscript="", bpl=[], gds={}):
 class PwnTube(tube):
     """ PwnT00ls Tube """
 
-    def dbg(self, gdbscript="", bpl=[], gds={}):
-        """ reference: https://github.com/pullp/pwn_framework
+    def dbg(self, gdbscript="", gds: dict = {}, bpl: list = []):
+        """
         @param bpl: break point list
         @param gds: gdb debug symbols
         """
 
+        # pass local mode
         if hasattr(self, "process_mode"):
             if self.process_mode != "local":
                 log.warning(f"you are running gdb with {self.process_mode} mode")
                 pause()
                 return
-            else:
-                pass
-        else:
-            pass
+
+        # set context.terminal
+        if os.getenv("TMUX"):
+            if len(context.terminal) == 0:
+                split_horizon = os.get_terminal_size().columns - 90
+                context.terminal = ["tmux", "sp", "-h", "-l", str(split_horizon)]
+                log.info(f"set `context.terminal = {str(context.terminal)}`")
 
         script_lines = list()
+
+        # add gdb debug symbols
+        for k, v in gds.items():
+            s = "set ${k}={v}".format(k=k, v=str(v))
+            script_lines.append(s)
 
         # add break point list
         for b in bpl:
             s = "b *{b}".format(b=str(b))
             script_lines.append(s)
 
-        # add gdb debug symbol
-        for k, v in gds.items():
-            s = "set ${k}={v}".format(k=k, v=str(v))
-            script_lines.append(s)
-
         script_lines.append(gdbscript)
         res = "\n".join(script_lines)
 
-        log.info(f"exec gdb script:\n{res}")
+        # log.info(f"exec gdb script:\n{res}")
         gdb.attach(self, res)
 
 
