e3006f1b1794ab75ece8df0701dbe2585e2fa995
the-soloist <46804083+the-soloist@users.noreply.github.com>
2022-08-08 21:45:14 +0800

    Update argparse config

--- lib/config.py
+++ lib/config.py
@@ -10,16 +10,16 @@ PT_PATH = Path(__file__).parent.parent
 def init_parser() -> argparse.ArgumentParser:
     # init parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--argument", action="store")  # for test
-    parser.add_argument("-d", "--debug", action="store_true")
-    parser.add_argument("-e", "--env", action="store_true")
-    parser.add_argument("-l", "--local", action="store_true")
-    parser.add_argument("-r", "--remote", action="store_true")
+    # parser.add_argument("-a", "--argument", action="store")  # for test
 
-    # sub parser
+    # init group
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-d", "--debug", action="store_true")
+    group.add_argument("-l", "--local", action="store_true")
+    group.add_argument("-r", "--remote", action="store_true")
+
+    # init awd sub parser
     subparsers = parser.add_subparsers(help='sub command')
-
-    # awd parser
     awd_parsers = subparsers.add_parser('awd', help='awd help')
     awd_parsers.add_argument('-tl', "--target_list", action="store", default=None, help='add target list')
 
