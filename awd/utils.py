9b53295eba4396e886c89b19105fcb1d65c2491a
Th3S <46804083+the-soloist@users.noreply.github.com>
2023-08-30 22:52:37 +0800

    refactor: update awd module

--- awd/utils.py
+++ awd/utils.py
@@ -1,38 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
 from pathlib import Path
-from pwn import context
+from pwn import context, remote
 from pwnutils.lib.log import plog
-
 
 __all__ = [
     "run"
 ]
 
 
-def run(args, exploit_fn, submit_fn=None, save_fp=None):
-    target_list = json.load(open(args.target_list, "r"))
+def parse_target_list(path: Path):
+    fp = open(path, "r")
+    content = fp.read().strip().split("\n")
+    fp.close()
+    return [tuple(x.split(":")) for x in content if x]
 
-    for hp in target_list:
-        host, port = hp.values()
+
+def run(args, exploit_fn, submit_fn=None, target_list=[], log_fp=None):
+    failed_list = []
+
+    if len(target_list) == 0:
+        plog.warning("no host in target list")
+        return failed_list
+
+    for target in target_list:
+        host, port = target
         plog.info(f"exploiting {host}:{port}")
+        context.log_level = "debug"
+
+        get_flag_status = False
+        submit_status = False
 
         try:
-            context.log_level = "warning"
-            sh = remote(host, port)
-            flag = exploit_fn(sh)
-            sh.close()
-            context.log_level = "info"
+            io = remote(host, port)
+            flag = exploit_fn(io)
+            io.close()
 
+            get_flag_status = True
             plog.success(f"get flag success: {flag}")
         except Exception as e:
             plog.warning(f"get flag failed, error: {e}")
+            failed_list.append((host, port))  # 获取flag失败
             continue
 
-        if not flag:
+        if not get_flag_status:
             plog.warning(f"not found flag")
+            failed_list.append((host, port))  # 获取空flag
             continue
 
         if submit_fn and flag:
@@ -42,7 +56,9 @@ def run(args, exploit_fn, submit_fn=None, save_fp=None):
             except Exception as e:
                 plog.warning(f"submit failed, error: {e}")
 
-        if save_fp and flag:
-            save_fp.write(f"{host}:{port} -> {flag}\n")
-        elif save_fp and not flag:
-            save_fp.write(f"{host}:{port} not found flag\n")
+        if log_fp and flag:
+            log_fp.write(f"{host}:{port} -> {flag}\n")
+        elif log_fp and not flag:
+            log_fp.write(f"{host}:{port} not found flag\n")
+
+    return failed_list
